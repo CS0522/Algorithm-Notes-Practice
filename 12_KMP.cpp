9dc16b2f16809620f3812c860f31a7162b47a6a1
hideaway_cs <812359326@qq.com>
2024-08-15 11:53:09 +0800

    Update 12_KMP.cpp

--- 12_KMP.cpp
+++ 12_KMP.cpp
@@ -25,57 +25,62 @@ void init()
 }
 
 // 计算 next 数组
-void calNext()
-{
-    // pointer of pattern string
-    int j = -1;
-    // nextArray[0] = -1
-    nextArray[0] = -1;
-    // 计算 nextArray[1] ~ nextArray[length - 1]
-    for (int i = 1; i < pattern.length(); i++)
+    void calNext(int *next, string &pattern)
     {
-        // 当前 pattern[i] != pattern[j + 1]
-        // j pointer 不断回溯 j == nextArray[j]
-        // 其实也就是 j pointer 回溯到上一个以相同字母开头的前缀的下标
-        while (j != -1 && pattern[i] != pattern[j + 1])
+        // 回溯指针
+        // 也可以表示最长前缀长度
+        int j = -1;
+        // 默认 next[0] = -1
+        next[0] = -1;
+        for (int i = 1; i < pattern.length(); i++)
         {
-            j = nextArray[j];
+            // 当正在匹配的字符 p[i] 
+            // 与正在匹配的最长前缀的下一位 p[j + 1] 
+            // 匹配失败时，
+            // j 回溯到最长前缀的上一个最长前缀
+            while (j != -1 && pattern[i] != pattern[j + 1])
+            {
+                j = next[j];
+            }
+            // 匹配成功
+            // 最长前缀 + 1
+            if (pattern[i] == pattern[j + 1])
+            {
+                j += 1;
+            }
+            next[i] = j;
         }
-        // 跳出循环
-        // 当前 pattern[i] == pattern[j + 1]
-        if (pattern[i] == pattern[j + 1])
-        {
-            j += 1;
-        }
-        // 赋值
-        nextArray[i] = j;
     }
-}
 
-// KMP
-bool KMP()
-{
-    int j = -1;
-    // 开始匹配
-    for (int i = 0; i < text.length(); i++)
+    // KMP
+    // 返回匹配成功的下标
+    int KMP(string &text, string &pattern)
     {
-        while (j != -1 && text[i] != pattern[j + 1])
+        int next[pattern.length()];
+        // 计算 next 数组
+        calNext(next, pattern);
+        int j = -1;
+        // 开始匹配
+        for (int i = 0; i < text.length(); i++)
         {
-            j = nextArray[j];
+            while (j != -1 && text[i] != pattern[j + 1])
+            {
+                j = next[j];
+            }
+            if (text[i] == pattern[j + 1])
+            {
+                j++;
+            }
+            // 当 j 匹配到模式串最后一位
+            // 匹配成功
+            if (j == pattern.length() - 1)
+            {
+                return i - j;
+            }
         }
-        if (text[i] == pattern[j + 1])
-        {
-            j++;
-        }
-        // succeeded
-        if (j == pattern.length() - 1)
-        {
-            return true;
-        }
+        // 匹配失败
+        return -1;
     }
-    // failed
-    return false;
-}
 
 // KMP + pattern出现次数
 int calKMPCount()
